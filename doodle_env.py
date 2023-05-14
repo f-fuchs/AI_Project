2377d4210fb279f9524ba025a523bc19785a933f
f-fuchs <fabianfuchs1999@googlemail.com>
2023-05-14 15:59:34 -0500

    initial commit

---
+++ doodle_env.py
@@ -0,0 +1,101 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import spaces
+
+from web_driver import DoodleJumpWebDriver
+
+
+class DoodleJumpEnv(gym.Env):
+    # Because of google colab, we cannot implement the GUI ('human' render mode)
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+
+    last_score_frame = np.empty((50, 20))
+
+    # observation space
+    # observation_space = Box(
+    #     low=0,
+    #     high=255,
+    #     shape=(4, 200, 300),
+    #     dtype=np.uint8
+    # )
+
+    def __init__(self, frame_time):
+        super(DoodleJumpEnv, self).__init__()
+
+        self.dJWB = DoodleJumpWebDriver(frame_time)
+        # time.sleep(2)
+        # self.dJWB.start_game()
+
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions, we have two: left and right
+        n_actions = 4
+        self.action_space = spaces.Discrete(n_actions)
+        self.actions = {
+            0: self.dJWB.move_left,
+            1: self.dJWB.move_right,
+            2: self.dJWB.stay_still,
+            3: self.dJWB.shoot,
+        }
+
+        # menu screen
+        self.menu = np.load("menu.npy")
+
+    def reset(self):
+        """
+        Important: the observation must be a numpy array
+        :return: (np.array)
+        """
+        self.dJWB.restart_game()
+        state = (
+            np.array(self.dJWB.get_screenshot_grayscale_rescaled(), dtype=np.single)
+            / 255,
+            np.array(self.dJWB.get_screenshot_grayscale_rescaled(), dtype=np.single)
+            / 255,
+            np.array(self.dJWB.get_screenshot_grayscale_rescaled(), dtype=np.single)
+            / 255,
+            np.array(self.dJWB.get_screenshot_grayscale_rescaled(), dtype=np.single)
+            / 255,
+        )
+        state = np.array(state)
+        return state
+
+    def step(self, action):
+        # apply chosen action
+        self.actions[action]()
+
+        # calculate reward
+        frame = self.dJWB.get_screenshot_grayscale_rescaled()
+        # frame.show()
+        new_score_frame = np.array(frame.crop((0, 0, 50, 20)))
+        if np.array_equal(new_score_frame, self.last_score_frame):
+            reward = 1
+        else:
+            reward = 0
+        self.last_score_frame = new_score_frame
+
+        # check if done
+        new_menu_frame = np.array(frame.crop((120, 210, 200, 250)))
+        done = np.array_equal(new_menu_frame, self.menu)
+
+        self.current_frame = frame
+        state = (
+            np.array(frame, dtype=np.single) / 255,
+            np.array(self.dJWB.get_screenshot_grayscale_rescaled(), dtype=np.single)
+            / 255,
+            np.array(self.dJWB.get_screenshot_grayscale_rescaled(), dtype=np.single)
+            / 255,
+            np.array(self.dJWB.get_screenshot_grayscale_rescaled(), dtype=np.single)
+            / 255,
+        )
+        state = np.array(state)
+        return state, reward, done
+
+    def render(self, mode):
+        if mode == "human":
+            self.current_frame.show()
+        elif mode == "rgb_array":
+            return np.array(self.current_frame)
+
+    def close(self):
+        pass
---
+++ web_driver.py
@@ -0,0 +1,93 @@
+import time
+from io import BytesIO
+
+import numpy as np
+from PIL import Image, ImageOps
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class DoodleJumpWebDriver:
+    def __init__(self, frame_time):
+        self.frame_time = frame_time
+        self.url = "https://doodle-jump.io/game/doodle-jump/"
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--window-size=200,300")
+        self.driver = webdriver.Chrome("chromedriver", options=options)
+        # self.driver.get(self.url)
+        self.action_chains = ActionChains(self.driver)
+        self.action_chain_left = (
+            self.action_chains.key_down(Keys.ARROW_LEFT)
+            .pause(frame_time)
+            .key_up(Keys.ARROW_LEFT)
+        )
+        self.action_chain_right = (
+            self.action_chains.key_down(Keys.ARROW_RIGHT)
+            .pause(frame_time)
+            .key_up(Keys.ARROW_RIGHT)
+        )
+        self.action_chain_shoot = (
+            self.action_chains.key_down(Keys.ARROW_UP)
+            .pause(frame_time)
+            .key_up(Keys.ARROW_UP)
+        )
+
+        self.play = np.load("play.npy")
+
+    def get_screenshot(self):
+        image_b64 = self.driver.get_screenshot_as_base64()
+        screen = Image.open(BytesIO(base64.b64decode(image_b64)))
+        return screen
+
+    def get_screenshot_grayscale(self):
+        return ImageOps.grayscale(self.get_screenshot())
+
+    def get_screenshot_grayscale_rescaled(self):
+        return ImageOps.grayscale(self.get_screenshot()).resize((84, 84))
+
+    def start_game(self):
+        elem_canvas = self.driver.find_element(By.XPATH, "/html/body/canvas")
+        self.action_chains.move_to_element(elem_canvas).move_by_offset(
+            -10, -75
+        ).click().perform()
+
+    def restart_game(self):
+        def __contains_play_button():
+            loading_frame = np.array(
+                self.get_screenshot_grayscale().crop((40, 72, 110, 97))
+            )
+            return np.array_equal(loading_frame, self.play)
+
+        while True:
+            self.driver.get(self.url)
+            # np.save("play.npy", np.array(self.get_screenshot_grayscale().crop((40, 72, 110, 97))))
+            i = 0
+            while i < 50 and not __contains_play_button():
+                time.sleep(0.1)
+                i += 1
+            if i < 50:
+                self.start_game()
+                return
+
+    def move_left(self):
+        self.action_chains.key_down(Keys.ARROW_LEFT).pause(self.frame_time).key_up(
+            Keys.ARROW_LEFT
+        ).perform()
+
+    def move_right(self):
+        self.action_chains.key_down(Keys.ARROW_RIGHT).pause(self.frame_time).key_up(
+            Keys.ARROW_RIGHT
+        ).perform()
+
+    def stay_still(self):
+        time.sleep(self.frame_time)
+
+    def shoot(self):
+        self.action_chains.key_down(Keys.ARROW_UP).pause(self.frame_time).key_up(
+            Keys.ARROW_UP
+        ).perform()
